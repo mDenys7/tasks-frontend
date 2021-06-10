3aef8eca5f077c80066d17895f648b24bd3e8632
mDenys7 <milton.silva17@fatec.sp.gov.br>
2021-06-10 00:08:22 -0300

    teste

--- src/main/java/br/ce/wcaquino/tasksfrontend/controller/TasksController.java
+++ src/main/java/br/ce/wcaquino/tasksfrontend/controller/TasksController.java
@@ -73,7 +73,7 @@ public class TasksController {
 		return "index";
 	}
 
-	
+	//teste
 	@SuppressWarnings("unchecked")
 	private List<Todo> getTodos() {
 		RestTemplate restTemplate = new RestTemplate();
